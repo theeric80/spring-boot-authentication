afa212c763024fa622536f3c09721b04e54fefe5
Eric Tsai <chunchieh@gmail.com>
2020-03-27 12:17:11 +0800

    add unittest for AuthenticationService::login

--- src/test/java/org/theeric/auth/user/service/AuthenticationServiceTest.java
+++ src/test/java/org/theeric/auth/user/service/AuthenticationServiceTest.java
@@ -18,8 +18,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.theeric.auth.core.web.exception.ClientErrorException;
+import org.theeric.auth.dto.AuthToken;
+import org.theeric.auth.user.form.LoginForm;
 import org.theeric.auth.user.form.RegistrationForm;
 import org.theeric.auth.user.model.User;
+import org.theeric.auth.user.model.UserSession;
 import org.theeric.auth.user.repository.UserSessionDao;
 
 @ExtendWith(SpringExtension.class)
@@ -66,6 +69,7 @@ public class AuthenticationServiceTest {
     @Test
     public void whenRegisterOk_thenPasswordShouldBeHashed() {
         final String expected = "hashed_password";
+
         when(passwordEncoder.encode("password")).thenReturn(expected);
         when(userService.create(any())).then(i -> i.getArgument(0));
 
@@ -88,6 +92,54 @@ public class AuthenticationServiceTest {
         });
 
         assertThat(e.getStatus()).isEqualTo(HttpStatus.CONFLICT);
+    }
+
+    @Test
+    public void whenLoginOk_thenTokenShouldBeReturned() {
+        final String expected = "token";
+
+        final User user = new User();
+        user.setPassword("hashed_password");
+
+        final UserSession session = new UserSession();
+        session.setToken(expected);
+        session.setUser(user);
+
+        when(passwordEncoder.matches("password", "hashed_password")).thenReturn(true);
+        when(userService.findByRef(any())).thenReturn(Optional.of(user));
+        when(userSessionDao.save(any())).thenReturn(session);
+
+        final LoginForm form = new LoginForm();
+        form.setReference("test@gmail.com");
+        form.setPassword("password");
+
+        final AuthToken token = authenticationService.login(form);
+
+        assertThat(token.getToken_type()).isEqualTo("Bearer");
+        assertThat(token.getAccess_token()).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenUserNotExists_thenExceptionShouldBeThrown() {
+        final ClientErrorException e = Assertions.assertThrows(ClientErrorException.class, () -> {
+
+            authenticationService.login(new LoginForm());
+        });
+
+        assertThat(e.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void whenPasswordInvalid_thenExceptionShouldBeThrown() {
+        when(passwordEncoder.matches(any(), any())).thenReturn(false);
+        when(userService.findByRef(any())).thenReturn(Optional.of(new User()));
+
+        final ClientErrorException e = Assertions.assertThrows(ClientErrorException.class, () -> {
+
+            authenticationService.login(new LoginForm());
+        });
+
+        assertThat(e.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
 }
